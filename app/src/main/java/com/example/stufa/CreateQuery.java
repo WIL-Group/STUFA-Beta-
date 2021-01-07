ad5bb690f4cc5904451327ef37e43f18e966cf3c
jnrJack <ralebakengjnr@gmail.com>
2021-01-07 09:27:50 +0200

    Added Query and QueryAdapter for the recycler view then worked on the create query activity

--- app/src/main/java/com/example/stufa/CreateQuery.java
+++ app/src/main/java/com/example/stufa/CreateQuery.java
@@ -2,18 +2,31 @@ package com.example.stufa;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreateQuery extends AppCompatActivity {
+import java.util.ArrayList;
 
-    CheckBox cbBookAllowance, cbMealAllowance, cbAccomodationOrTransportAllowance;
+public class CreateQuery extends AppCompatActivity implements QueryAdapter.ItemClicked  {
+
+    CheckBox cbBookAllowance, cbMealAllowance, cbAccommodationOrTransportAllowance;
     EditText etQueryMessage;
     Button btnSave, btnDelete, btnSubmit;
+
+    RecyclerView recyclerView;
+    private QueryAdapter myAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Query> queries;
+    Query query;
+    String type, message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +41,56 @@ public class CreateQuery extends AppCompatActivity {
 
         cbBookAllowance = findViewById(R.id.cbBookAllowance);
         cbMealAllowance = findViewById(R.id.cbMealAllowance);
-        cbAccomodationOrTransportAllowance = findViewById(R.id.cbAccomodationOrTransportAllowance);
+        cbAccommodationOrTransportAllowance = findViewById(R.id.cbAccomodationOrTransportAllowance);
         etQueryMessage = findViewById(R.id.etQueryMessage);
         btnSave = findViewById(R.id.btnSave);
         btnDelete = findViewById(R.id.btnDelete);
         btnSubmit = findViewById(R.id.btnSubmit);
+        queries = new ArrayList<>();
 
+        recyclerView = findViewById(R.id.rvList);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(CreateQuery.this);
+        recyclerView.setLayoutManager(layoutManager);
+        myAdapter = new QueryAdapter(queries,CreateQuery.this);
+        recyclerView.setAdapter(myAdapter);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            if(cbBookAllowance.isChecked() || cbMealAllowance.isChecked() || cbAccommodationOrTransportAllowance.isChecked())
+            {
+                if(etQueryMessage.getText().toString().isEmpty())
+                {
+                    Toast.makeText(CreateQuery.this, "Please enter query message", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    if(!cbBookAllowance.getText().toString().isEmpty())
+                    {
+                        type = cbBookAllowance.getText().toString().trim();
+                    }
+                    else if(!cbMealAllowance.getText().toString().isEmpty())
+                    {
+                        type = cbMealAllowance.getText().toString().trim();
+                    }
+                    else if(!cbAccommodationOrTransportAllowance.getText().toString().isEmpty())
+                    {
+                        type = cbAccommodationOrTransportAllowance.getText().toString().trim();
+                    }
+
+                    message = etQueryMessage.getText().toString().trim();
+
+                    query = new Query(type, message);
+                    queries.add(query);
+                    Toast.makeText(CreateQuery.this, "Query succesfully added", Toast.LENGTH_SHORT).show();
+                }
 
             }
+
+            }
+
         });
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +106,11 @@ public class CreateQuery extends AppCompatActivity {
 
             }
         });
+    }
 
+
+    @Override
+    public void onItemClicked(int index) {
 
     }
 }
